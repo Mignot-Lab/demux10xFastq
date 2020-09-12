c59a6439877d97a58fc85936607a01a3b248b9ed
Aditya <ambati@stanford.edu>
2020-09-12 09:28:48 -0700

    support for arg parse

--- scripts/demuxFastq.py
+++ scripts/demuxFastq.py
@@ -3,18 +3,8 @@ import numpy as np
 from collections import defaultdict
 import os
 import gzip
-#2442687652
-libList = pd.read_csv('data/AggrFileListcDNAwithLib.csv')
-goodCells = pd.read_csv('data/ClassifiedDemuxedBarcodes_July23_2020.csv.gz')
-goodCells=goodCells.loc[(goodCells.barcodes != 'Negative') & (goodCells.barcodes != 'Doublet')]
-goodCells.reset_index(drop=True)
+import argparse
 
-## for each fastq library, iterate and make good cells
-goodCellSeries=goodCells.cellIds.str.split('.').to_list()
-goodCellSeries
-cellMaster=defaultdict(list)
-for i, j, in goodCellSeries:
-    cellMaster[j].append(i)
 
 
 def gzipHandle(fileName):
@@ -24,9 +14,9 @@ def gzipHandle(fileName):
         fileOut = open(fileName, 'rt')
     return fileOut
 
-def fileOutOp(sampleBC):
-    R1out = os.path.join('outs', sampleBC+'_'+'R1.fastq')
-    R2out = os.path.join('outs', sampleBC+'_'+'R2.fastq')
+def fileOutOp(sampleBC, libN):
+    R1out = os.path.join('outs', 'chunk'+libN,sampleBC+'_'+'R1.fastq')
+    R2out = os.path.join('outs', 'chunk'+libN,sampleBC+'_'+'R2.fastq')
     if os.path.exists(R1out) and os.path.exists(R2out):
         R1outHandle = open(R1out, 'a')
         R2outHandle = open(R2out, 'a')
@@ -36,7 +26,7 @@ def fileOutOp(sampleBC):
     return [R1outHandle, R2outHandle]
 
 
-def processFastq(R1, R2, libN, goodCells, cellMaster):
+def processFastq(R1, R2, libN, goodCellBCDict, cellMaster):
     #libN = '1'
     R1File = gzipHandle(R1)
     R2File = gzipHandle(R2)
@@ -59,18 +49,19 @@ def processFastq(R1, R2, libN, goodCells, cellMaster):
                 writeState = True
                 sampleBC = umiMaster.get(umi)
                 if sampleBC:
-                    R1out, R2out = fileOutOp(sampleBC)
+                    R1out, R2out = fileOutOp(sampleBC, libN)
                     r1String += r1
                     r2String += r2  
             else:
                 if cellBC in cellMaster.get(libN):
                     writeState = True
-                    sampleBC=goodCells.loc[goodCells.cellIds == cellBC+'.'+libN, 'barcodes'].to_list()[0]
+                    celKey= cellBC+'.'+libN
+                    assert celKey in goodCellBCDict
+                    sampleBC=goodCellBCDict.get(celKey)
                     if sampleBC:# == "ACATGCGT":
                         umiMaster[umi] = sampleBC
-                        #assert len(sampleBC) == 1
                         ## make file handler function here
-                        R1out, R2out = fileOutOp(sampleBC)
+                        R1out, R2out = fileOutOp(sampleBC, libN)
                         r1String += r1
                         r2String += r2  
                 else:
@@ -96,8 +87,29 @@ def processFastq(R1, R2, libN, goodCells, cellMaster):
     R1out.close()
     R2out.close()
 
-R1 = 'data/cDNA8_S88_L004_R1_001.fastq.gz'
-R2 = 'data/cDNA8_S88_L004_R2_001.fastq.gz'
-libN = '16'
+# R1 = 'data/cDNA8_S88_L004_R1_001.fastq.gz'
+# R2 = 'data/cDNA8_S88_L004_R2_001.fastq.gz'
+# libN = '16'
 
-processFastq(R1, R2, libN, goodCells, cellMaster)
+def main():
+    parser = argparse.ArgumentParser(description='Script to demultiplex base 10xFastq files into respective samples for downstream analyses')
+    parser.add_argument('-R1', help='FASTQ pair 1', required=True)
+    parser.add_argument('-R2', help='FASTQ pair 2', required=True)
+    parser.add_argument('-Lib', help='Library number', required=True)
+    parser.add_argument('-D', help='A csv file with 2 columns, cellIds and sample id of each cell')
+    args=parser.parse_args()
+    R1 = args.R1
+    R2 = args.R2
+    libN = str(args.Lib)
+    demuxed = args.D
+    goodCells = pd.read_csv(demuxed)#pd.read_csv('data/ClassifiedDemuxedBarcodes_July23_2020.csv.gz')
+    goodCells=goodCells.loc[(goodCells.barcodes != 'Negative') & (goodCells.barcodes != 'Doublet')]
+    goodCells.reset_index(drop=True, inplace=True)
+    goodCellBCDict={i:j for i, j in zip(goodCells.cellIds.values, goodCells.barcodes.values)}
+    ## for each fastq library, iterate and make good cells
+    goodCellSeries=goodCells.cellIds.str.split('.').to_list()
+    goodCellSeries
+    cellMaster=defaultdict(list)
+    for i, j, in goodCellSeries:
+        cellMaster[j].append(i)
+    processFastq(R1, R2, libN, goodCellBCDict, cellMaster)
